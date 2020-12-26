07b30a99781ce4cafa95a5cff217cf9301c6658a
Yoko <yokannnnn10@gmail.com>
2020-12-27 02:13:24 +0900

    20201227

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS employee (
     no INTEGER PRIMARY KEY,
     birthday DATE,
-    first_name VARCHAR(10),
-    last_name VARCHAR(10),
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
     mail_address VARCHAR(30),
     sex VARCHAR(10)
 );
